46b464ab0aaec87b24d5247b96c572c411663800
zaghir <zaghir.youssef@gmail.com>
2019-10-08 14:23:24 +0200

    first commit

---
+++ src/main/java/com/bd/eshopper/api/train/ApiTrainApplication.java
@@ -0,0 +1,13 @@
+package com.bd.eshopper.api.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bd/eshopper/api/train/ApiTrainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bd.eshopper.api.train;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiTrainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
